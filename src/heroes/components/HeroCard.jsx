424a0da262195219d47a1056588cc6e64aff4f37
LucasNF22 <lucasnfiorenitno@gmail.com>
2023-03-26 19:53:08 -0300

    Update HeroCard

---
+++ src/heroes/components/HeroCard.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+
+export const HeroCard = ({
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+}) => {
+  return (
+
+    <div className='col' >  
+        <div className='card' > 
+            <div className='row no-gutter' >
+                <img src="" alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/heroes/components/index.js
+++ src/heroes/components/index.js
@@ -1,2 +1,3 @@
 
 export * from './HeroeList';
+export * from './HeroCard';
